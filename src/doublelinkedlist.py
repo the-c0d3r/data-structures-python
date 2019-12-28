b879b4ed6bb8f38eb4c5fcd1f3e81b0e666fecd2
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2019-12-28 16:08:30 +0800

    Double Linked List completed

---
+++ src/doublelinkedlist.py
@@ -0,0 +1,64 @@
+from linkedlist import Node
+from linkedlist import LinkedList
+
+
+class DoubleLinkedNode(Node):
+    _prev = None
+
+    @property
+    def prev(self):
+        return self._prev
+
+    @prev.setter
+    def prev(self, value):
+        self._prev = value
+
+
+
+class DoubleLinkedList(LinkedList):
+
+    def insert(self, node: DoubleLinkedNode) -> None:
+        """Inserts the node into the list"""
+        if self._head is None:
+            self._head = node
+            self._last = node
+            self._pointer = node
+        else:
+            self._last.next = node
+            node.prev = self._last
+            self._last = node
+
+    def delete(self, node: DoubleLinkedNode) -> None:
+        """Deletes the given node from list"""
+        # Case when node does not exist
+        if node not in self:
+            return
+
+        # Case when node is the only node
+        if self.size == 1:
+            self._head = None
+            self._last = None
+            self._pointer = None
+            return
+
+        # Case when node is head
+        if node == self._head:
+            self._head = self._head.next
+            self._head.prev = None
+            self._pointer = self._head
+            return
+
+        # Case when node is last
+        if node == self._last:
+            # iterate to exhaust all the nodes and get the last node before
+            self._last = self._last.prev
+            self._last.next = None
+
+        # Case when node is in the middle
+        ptr = self._head
+        while ptr.next:
+            ptr = ptr.next
+            if ptr == node:
+                ptr.prev.next = ptr.next
+                ptr.next.prev = ptr.prev
+                return
---
+++ tests/test_double_linked_list.py
@@ -0,0 +1,102 @@
+import pytest
+
+from src.doublelinkedlist import DoubleLinkedNode, DoubleLinkedList
+
+
+@pytest.fixture
+def node():
+    return DoubleLinkedNode()
+
+
+@pytest.fixture
+def doublelinkedlist():
+    return DoubleLinkedList()
+
+
+class TestNode:
+    def test_prev(self):
+        node1 = DoubleLinkedNode(42)
+        node2 = DoubleLinkedNode(43)
+
+        node1.next = node2
+        node2.prev = node1
+
+        assert node1.next == node2
+        assert node2.prev == node1
+        assert node2.prev.value == 42
+
+
+class TestDoubleLinkedList:
+    def test_init(self, doublelinkedlist):
+        assert doublelinkedlist is not None
+        assert doublelinkedlist.head is None
+        assert doublelinkedlist.last is None
+
+    def test_insert(self, doublelinkedlist, node):
+        node.value = 42
+        doublelinkedlist.insert(node)
+        assert doublelinkedlist.size == 1
+        assert doublelinkedlist.head == node
+        assert doublelinkedlist.last == node
+        assert doublelinkedlist.head.value == node.value
+        assert doublelinkedlist.last.value == node.value
+
+    def test_insert_bulk(self, doublelinkedlist):
+        nodelist = [DoubleLinkedNode(_) for _ in range(10)]
+        for node in nodelist:
+            doublelinkedlist.insert(node)
+
+        assert doublelinkedlist.size == 10
+        assert doublelinkedlist.last == nodelist[-1]
+        assert doublelinkedlist.head == nodelist[0]
+
+        assert doublelinkedlist.last.prev == nodelist[-2]
+
+    def test_delete_head(self, doublelinkedlist):
+        node1 = DoubleLinkedNode(1)
+        node2 = DoubleLinkedNode(2)
+        node3 = DoubleLinkedNode(3)
+
+        doublelinkedlist.insert(node1)
+        doublelinkedlist.insert(node2)
+        doublelinkedlist.insert(node3)
+
+        assert doublelinkedlist.size == 3
+
+        doublelinkedlist.delete(node1)
+        assert doublelinkedlist.size == 2
+        assert doublelinkedlist.head.next == node3
+        assert doublelinkedlist.last.prev == node2
+
+    def test_delete_mid(self, doublelinkedlist):
+        node1 = DoubleLinkedNode(1)
+        node2 = DoubleLinkedNode(2)
+        node3 = DoubleLinkedNode(3)
+
+        doublelinkedlist.insert(node1)
+        doublelinkedlist.insert(node2)
+        doublelinkedlist.insert(node3)
+
+        assert doublelinkedlist.size == 3
+
+        doublelinkedlist.delete(node2)
+
+        assert doublelinkedlist.size == 2
+        assert doublelinkedlist.head.next == node3
+        assert doublelinkedlist.last.prev == node1
+
+    def test_delete_tail(self, doublelinkedlist):
+        node1 = DoubleLinkedNode(1)
+        node2 = DoubleLinkedNode(2)
+        node3 = DoubleLinkedNode(3)
+
+        doublelinkedlist.insert(node1)
+        doublelinkedlist.insert(node2)
+        doublelinkedlist.insert(node3)
+
+        assert doublelinkedlist.size == 3
+
+        doublelinkedlist.delete(node3)
+        assert doublelinkedlist.size == 2
+        assert doublelinkedlist.head.next == node2
+        assert doublelinkedlist.last.prev == node1
