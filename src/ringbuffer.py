af74f33e0e8d98987333df15c44b6962b3a2566a
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2021-01-16 22:45:59 +0800

    ringbuffer.py: updated the index increment with modulus operation

--- src/ringbuffer.py
+++ src/ringbuffer.py
@@ -16,27 +16,15 @@ class RingBuffer:
         self.write_index: int = 0
         self.ring: List[Element] = [Element(None)] * self.size
 
-    def incr_read_index(self):
-        """increment the read index"""
-        self.read_index += 1
-        if self.read_index == self.size:
-            self.read_index = 0
-
-    def incr_write_index(self):
-        """increment the write index"""
-        self.write_index += 1
-        if self.write_index == self.size:
-            self.write_index = 0
-
     def enqueue(self, value: Any) -> None:
         """enqueue the value into the ring buffer"""
         self.ring[self.write_index].value = value
-        self.incr_write_index()
+        self.write_index = (self.write_index + 1) % self.size
 
     def dequeue(self) -> Any:
         """dequeue the value from the ring buffer"""
         value = self.ring[self.read_index].value
         self.ring[self.read_index].value = SENTINEL
-        self.incr_read_index()
+        self.read_index = (self.read_index + 1) % self.size
 
         return value
